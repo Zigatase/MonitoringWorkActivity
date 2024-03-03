9fd34f5855dee41a97982e21afa9f6d9486e52bf
Ziglot <zigatase@gmail.com>
2024-03-03 12:22:42 +0300

    Добавьте файлы проекта.

---
+++ MonitoringWorkActivityC/client.h
@@ -0,0 +1,10 @@
+#ifndef MONITORINGWORKACTIVITYC_CLIENT_H_
+#define MONITORINGWORKACTIVITYC_CLIENT_H_
+
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#endif // !MONITORINGWORKACTIVITYC_CLIENT_H_
---
+++ MonitoringWorkActivityS/server.h
@@ -0,0 +1,11 @@
+#ifndef MONITORINGWORKACTIVITYS_SERVER_H_
+#define MONITORINGWORKACTIVITYS_SERVER_H_
+
+#include <iostream>
+#include <string>
+#include <ws2tcpip.h>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#endif // !MONITORINGWORKACTIVITYS_SERVER_H_
