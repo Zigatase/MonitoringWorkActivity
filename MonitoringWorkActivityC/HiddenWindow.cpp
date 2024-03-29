1871c8f9a755a657a416d23227231fcc7484c14f
Zigatase <120517445+Zigatase@users.noreply.github.com>
2024-03-29 13:41:36 +0300

    Update HiddenWindow.cpp

--- MonitoringWorkActivityC/HiddenWindow.cpp
+++ MonitoringWorkActivityC/HiddenWindow.cpp
@@ -66,55 +66,92 @@ void main()
 }
 
 
-bool TakeScreenshot(HWND hDesktop, const wstring& filename) {
-    // Получение размеров рабочего стола
-    RECT rect;
-    GetWindowRect(hDesktop, &rect);
 
-    // Создание контекста устройства для рабочего стола
-    HDC hdcDesktop = GetDCEx(hDesktop, NULL, DCX_WINDOW);
-    if (hdcDesktop == NULL) {
-        cout << "Ошибка получения контекста устройства!" << endl;
-        return false;
-    }
+Чат с Gemini
+Фото профиля
+Создай невидимый рабочий стол с помощь C++ и WinApi не используя сторонние библиотеки, а так же сделай так что бы программа сделала скриншот невидимого рабочего стола со всеми открытыми окнами
+Показать черновики
 
-    // Создание совместимого контекста устройства
-    HDC hdcMem = CreateCompatibleDC(hdcDesktop);
-    if (hdcMem == NULL) {
-        cout << "Ошибка создания совместимого контекста устройства!" << endl;
-        return false;
-    }
 
-    // Создание битмапа
-    HBITMAP hBitmap = CreateDIBSection(hdcDesktop, (BITMAPINFO*)&BITMAPINFO{ sizeof(BITMAPINFOHEADER), rect.right - rect.left, rect.bottom - rect.top, 1, 32, BI_RGB, 0 }, DIB_RGB_COLORS, NULL, NULL, 0);
-    if (hBitmap == NULL) {
-        cout << "Ошибка создания битмапа!" << endl;
-        return false;
-    }
 
-    // Выбор битмапа в контекст устройства
-    SelectObject(hdcMem, hBitmap);
 
-    // Копирование изображения рабочего стола в битмап
-    BitBlt(hdcMem, 0, 0, rect.right - rect.left, rect.bottom - rect.top, hdcDesktop, 0, 0, SRCCOPY);
 
-    // Сохранение битмапа в JPEG-файл
-    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-    ULONG_PTR gdiplusToken;
-    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
-    CLSID encoderClsid;
-    GetEncoderClsid(L"image/jpeg", &encoderClsid);
 
-    Gdiplus::Bitmap bitmap(hBitmap);
-    bitmap.Save(filename.c_str(), &encoderClsid, NULL);
 
-    GdiplusShutdown(gdiplusToken);
 
-    // Освобождение ресурсов
-    DeleteObject(hBitmap);
-    DeleteDC(hdcMem);
-    DeleteDC(hdcDesktop);
 
-    return true;
+
+
+
+Создание невидимого рабочего стола и скриншот с помощью C++ и WinApi
+
+Создание невидимого рабочего стола:
+
+C++
+#include <windows.h>
+
+int main() {
+  // Создаем новый виртуальный рабочий стол
+  HDESK hDesktop = CreateDesktop("Невидимый рабочий стол", NULL, NULL, 0, DESKTOP_READOBJECTS | DESKTOP_WRITEOBJECTS, NULL);
+  if (hDesktop == NULL) {
+    return 1;
+  }
+
+  // Переключаемся на новый рабочий стол
+  BOOL bSwitch = SwitchDesktop(hDesktop);
+  if (!bSwitch) {
+    return 1;
+  }
+
+  // Выводим сообщение на невидимый рабочий стол
+  HWND hWnd = GetConsoleWindow();
+  HDC hdc = GetDC(hWnd);
+  TextOut(hdc, 0, 0, "Привет с невидимого рабочего стола!", 32);
+  ReleaseDC(hWnd, hdc);
+
+  // Скриншот невидимого рабочего стола
+  HDC hdcScreenshot = CreateDC(NULL, "DISPLAY", NULL, NULL);
+  HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreenshot, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
+  HGDIOBJ hOldBitmap = SelectObject(hdcScreenshot, hBitmap);
+  BitBlt(hdcScreenshot, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), hdc, 0, 0, SRCCOPY);
+  SelectObject(hdcScreenshot, hOldBitmap);
+
+  // Сохранение скриншота
+  BITMAP bmpInfo;
+  GetObject(hBitmap, sizeof(BITMAP), &bmpInfo);
+  BITMAPFILEHEADER bmfh;
+  ZeroMemory(&bmfh, sizeof(BITMAPFILEHEADER));
+  bmfh.bfType = 0x4D42; // "BM"
+  bmfh.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + bmpInfo.bmWidthBytes * bmpInfo.bmHeight;
+  bmfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+  BITMAPINFO bi;
+  ZeroMemory(&bi, sizeof(BITMAPINFO));
+  bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+  bi.bmiHeader.biWidth = bmpInfo.bmWidth;
+  bi.bmiHeader.biHeight = bmpInfo.bmHeight;
+  bi.bmiHeader.biPlanes = 1;
+  bi.bmiHeader.biBitCount = bmpInfo.bmBitsPixel;
+  bi.bmiHeader.biCompression = BI_RGB;
+
+  HANDLE hFile = CreateFile("скриншот.bmp", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (hFile != INVALID_HANDLE_VALUE) {
+    DWORD dwWritten;
+    WriteFile(hFile, &bmfh, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+    WriteFile(hFile, &bi, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
+    WriteFile(hFile, bmpInfo.bmBits, bmpInfo.bmWidthBytes * bmpInfo.bmHeight, &dwWritten, NULL);
+    CloseHandle(hFile);
+  }
+
+  // Очистка
+  DeleteObject(hBitmap);
+  DeleteDC(hdcScreenshot);
+
+  // Переключаемся обратно на основной рабочий стол
+  SwitchDesktop(GetThreadDesktop(GetCurrentThreadId()));
+
+  // Закрываем виртуальный рабочий стол
+  CloseDesktop(hDesktop);
+
+  return 0;
 }
